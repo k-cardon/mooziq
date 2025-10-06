9c945ea7414e119a7f8cd8190069bf43475ea33e
k-cardon <cardon.kristen.n@gmail.com>
2025-10-06 11:46:12 +0200

    update json parsing

--- Assignment_2_Skeleton/top_tracks.py
+++ Assignment_2_Skeleton/top_tracks.py
@@ -12,23 +12,19 @@ def get_top_tracks():
     with open('./dataset/top_tracks/0L8ExT028jH3ddEcZwqJJ5.json', 'r') as file:
         all_tracks = json.load(file)
         
-        for listing in all_tracks.values():
-            for item in listing:
-                # print(f'variable listing = {item}')
-                # print(f'variable listing type = {type(item)}')
-                name = item['name']
-                popularity_rank = item['popularity']
-                if popularity_rank <= 30:
-                    popularity_message = 'No one knows this song.'
-                elif popularity_rank <= 50:
-                    popularity_message = 'Popular song.'
-                elif popularity_rank <= 70:
-                    popularity_message = 'It is quite popular now!'
-                else:
-                    popularity_message = 'It is made for the charts!'
-                # print(name)
-                # print(popularity_rank)
+    for track in all_tracks['tracks']:
+        name = track['name']
+        popularity_rank = track['popularity']
+        if popularity_rank <= 30:
+            popularity_message = 'No one knows this song.'
+        elif popularity_rank <= 50:
+            popularity_message = 'Popular song.'
+        elif popularity_rank <= 70:
+            popularity_message = 'It is quite popular now!'
+        else:
+            popularity_message = 'It is made for the charts!'
 
-                print(f'- \'{name}\' has a popularity score of {popularity_rank}. {popularity_message}')
+
+        print(f'- \'{name}\' has a popularity score of {popularity_rank}. {popularity_message}')
 
 get_top_tracks()
