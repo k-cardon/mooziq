56e206539826962c728698b67c34df8bf363cfec
Piyanont <piyanont@hotmail.com>
2025-10-12 13:24:42 +0200

    Update task_1_artists.py
    Fixed some issues:
    - sorted all the list
    - used dictionary instead of list 
    - used id for key in dictionary to be used for another task

--- Assignment_2_Skeleton/task_1_artists.py
+++ Assignment_2_Skeleton/task_1_artists.py
@@ -1,30 +1,34 @@
-# This is where the entry point of your solution should be
-
-#task 1 
 
 import json
 import os
 
 def get_all_artists():
-    #TODO: consider making artists a dictionary with key ID instead of a list
-    #TODO: sort the files by name before processing them. You are allowed to use the Python functions sorted() or .sort() for this task.
-    
-    artists = []
-    folder = "dataset/artists"
 
-    for filename in os.listdir(folder):
+    artists = {}
+
+    folder = "dataset/artists"
+    filenames = os.listdir(folder)
+    filenames.sort()
+
+    for filename in filenames:
         if filename.endswith(".json"):
             filepath = os.path.join(folder, filename)
             with open(filepath, "r", encoding="utf-8") as file:
                 artist = json.load(file)
-                artists.append(artist)
+                artist_id = artist.get("id")
+                if artist_id:
+                    artists[artist_id] = artist
     
     return artists
 
-#TODO: make this a function
-all_artists = get_all_artists()
+def show_artists():
+        
+    all_artists = get_all_artists()
 
-print(f"Artists found in the database: ")
+    print(f"Artists found in the database: ")
 
-for artist in all_artists:
-    print(f"- {artist['name']}")
+    for artist_id, artist in all_artists.items():
+        print(f"- {artist['name']}")
+
+if __name__ == "__main__":
+    show_artists()
